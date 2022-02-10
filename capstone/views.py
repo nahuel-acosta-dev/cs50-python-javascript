637cd6d64c46a6bf1124fc706225dcf808036394
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-02-09 22:06:09 -0300

    react front-end environment created

--- capstone/views.py
+++ capstone/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .models import User
 
 # Create your views here.
+
+
+def api(request):
+    pass
