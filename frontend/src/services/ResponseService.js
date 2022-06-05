c1cfde5a0eb93beb230227afafaa8f85c43d4180
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-06-04 21:58:36 -0300

    reload

--- frontend/src/services/ResponseService.js
+++
@@ -1,9 +0,0 @@
-
-const ResponseService = (response, setItem, logoutUser) => {
-    if(response.status === 200)setItem(response.data);
-    else if(response.statusText === 'Unauthorized')logoutUser();
-}
-
-export default { 
-    Response:ResponseService,
-  }
